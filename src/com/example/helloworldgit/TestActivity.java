87071811eaa41f7b0a5b8547cdea4f3fc47d15fe
coffer <clever4vincent@gmail.com>
2016-08-15 11:24:29 +0800

    update test onCreate

--- src/com/example/helloworldgit/TestActivity.java
+++ src/com/example/helloworldgit/TestActivity.java
@@ -1,7 +1,11 @@
 package com.example.helloworldgit;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class TestActivity extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
 }
